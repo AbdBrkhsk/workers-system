86f5779da8211b4e85758c6e71e27468917618e2
abd brk <abdbrkhsh@gmail.com>
2024-10-04 17:37:28 +0300

    install jwt and add admin auth

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\AdminController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -9,11 +11,19 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group([
+    'middleware' => ['api'],
+    'prefix' => 'auth/admin'
+
+], function ($router) {
+    Route::post('/login', [AdminController::class, 'login']);
+    Route::post('/register', [AdminController::class, 'register']);
+    Route::post('/logout', [AdminController::class, 'logout']);
+    Route::post('/refresh', [AdminController::class, 'refresh']);
+    Route::get('/user-profile', [AdminController::class, 'userProfile']);    
 });
